832a4f00f10e613732e8f1c6aa119c7d02b4357f
steward-fu <g9313716@yuntech.edu.tw>
2018-07-17 22:34:55 +0800

    updated app2bin

--- app2bin/main.c
+++ app2bin/main.c
@@ -52,14 +52,45 @@ typedef struct {
 	uint32_t offset;
 } __attribute__((__packed__)) _app_expt_entry;
 
+uint32_t print_table(uint8_t *src, uint32_t off, uint32_t size)
+{
+  // find string table
+  uint8_t *p = src + off;
+  uint32_t str_off = off;
+  uint32_t cnt, total = size/sizeof(_app_impt_entry);
+	for(cnt=0; cnt<total; cnt++){
+    if(((_app_impt_entry*)p)->unknown[1] > 0x20000){
+      printf("  string: count(0x%x) offset(0x%x)\n", cnt, str_off);
+      break;
+    }
+		p+= sizeof(_app_impt_entry);
+    str_off+= sizeof(_app_impt_entry);
+	}
+
+  // print each import item
+	p = src + off;
+	for(cnt=0; cnt<total; cnt++, p+= sizeof(_app_impt_entry)){
+    if(((_app_impt_entry*)p)->unknown[1] == 0x00000){
+      continue;
+    }
+    if(((_app_impt_entry*)p)->unknown[1] > 0x20000){
+      break;
+    }
+		printf("  0x%x(%s)\n", ((_app_impt_entry*)p)->offset, &src[((_app_impt_entry*)p)->str_offset + str_off]);
+	}
+  return str_off;
+}
+
 int main(int argc, char** argv)
 {
+  // check argument
 	printf("decode dingoo app back to binary file\n");
 	if(argc != 2){
 		printf("usage:\n  app2bin test.app\n");
 		return -1;
 	}
-  
+ 
+  // read xxx.app
 	FILE* file = fopen(argv[1], "rb");
   if(file == NULL) {
     printf("failed to open app: %s", argv[1]);
@@ -78,13 +109,14 @@ int main(int argc, char** argv)
   fread(body, 1, len, file);
   fclose(file);
 
-	printf("parsing app...\n");
-	
+  // parsing
+	printf("parsing app...\n");	
 	_app_ccdl ccdl;
 	_app_impt impt;
 	_app_expt expt;
 	_app_rawd rawd;
 
+  // ccdl table
 	uint8_t *ptr = body;
 	if(memcmp(ptr, "CCDL", 4) != 0){
 		printf("invalid app file format (miss CCDL section)\n");
@@ -92,30 +124,29 @@ int main(int argc, char** argv)
 	}
 	ptr+= sizeof(_app_ccdl);
 
-	uint32_t cnt=0;
+  // import table
+	uint32_t cnt=0, total=0;
 	uint32_t off = ((_app_impt*)ptr)->offset;
 	uint32_t size = ((_app_impt*)ptr)->size;
 	if(memcmp(ptr, "IMPT", 4) != 0){
 		printf("invalid app file format (miss IMPT section)\n");
 		goto err;
 	}
-	printf("import offset: %d\n", off);
-	printf("import size: %d\n", size);
-	/*
-	uint8_t *p = body + off;
-	for(cnt=0; cnt<size/sizeof(_app_impt_entry); cnt++){
-		printf("  %x: 0x%x\n", ((_app_impt_entry*)p)->str_offset, ((_app_impt_entry*)p)->offset);
-		p+= sizeof(_app_impt_entry);
-	}
-	*/
+	printf("import offset: 0x%x\n", off);
+	printf("import size: 0x%x\n", size);
+  print_table(body, off, size);
 	ptr+= sizeof(_app_impt);
 
+  // export table
 	if(memcmp(ptr, "EXPT", 4) != 0){
 		printf("invalid app file format (miss EXPT section)\n");
 		goto err;
 	}
-	printf("export offset: %d\n", ((_app_expt*)ptr)->offset);
-	printf("export size: %d\n", ((_app_expt*)ptr)->size);
+	off = ((_app_rawd*)ptr)->offset;
+	size = ((_app_rawd*)ptr)->size;
+	printf("export offset: %d\n", off);
+	printf("export size: %d\n", size);
+  print_table(body, off, size);
 	ptr+= sizeof(_app_expt);
 	
 	if(memcmp(ptr, "RAWD", 4) != 0){
@@ -123,6 +154,7 @@ int main(int argc, char** argv)
 		goto err;
 	}
 
+  // raw table
 	off = ((_app_rawd*)ptr)->offset;
 	size = ((_app_rawd*)ptr)->size;
 	printf("raw offset: %d\n", off);
